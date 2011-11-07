af7075835a9b702fdb8b8205251e6a0b73b41d4a
Christian Moscardi <clm2183@columbia.edu>
2011-11-06 22:12:43 -0500

    theory 1 done

--- src/com/theory/DormRoom.java
+++ src/com/theory/DormRoom.java
@@ -1,6 +1,14 @@
 package com.theory;
 
-public class DormRoom implements Comparable<DormRoom>{
+import java.util.Comparator;
+
+/**
+ * A super cool DormRoom
+ * Compares by Occupancy
+ * @author Christian
+ *
+ */
+public class DormRoom implements Comparable<DormRoom> {
 	private int occupancy;
 	private int floorNumber;
 	public DormRoom(int occupancy, int floorNumber) {
@@ -8,6 +16,9 @@ public class DormRoom implements Comparable<DormRoom>{
 		this.floorNumber=floorNumber;
 	}
 	
+	/**
+	 * Compare by occupancy
+	 */
 	public int compareTo(DormRoom other){
 		if(occupancy>other.getOccupancy()){
 			return 1;
@@ -24,5 +35,33 @@ public class DormRoom implements Comparable<DormRoom>{
 	
 	public int getFloorNumber(){
 		return floorNumber;
+	}
+
+	public static Comparator<DormRoom> compareByOccupancy(){
+		return new Comparator<DormRoom>(){
+			public int compare(DormRoom a, DormRoom b){
+				if(a.getOccupancy()>b.getOccupancy()){
+					return 1;
+				}
+				else if(a.getOccupancy()==b.getOccupancy()){
+					return 0;
+				}
+				else return -1;
+			}
+		};
+	}
+
+	public static Comparator<DormRoom> compareByFloorNumber(){
+		return new Comparator<DormRoom>(){
+			public int compare(DormRoom a, DormRoom b){
+				if(a.getFloorNumber()>b.getFloorNumber()){
+					return 1;
+				}
+				else if (a.getFloorNumber()==b.getFloorNumber()){
+					return 0;
+				}
+				else return -1;
+			}
+		};
 	}
 }
--- src/com/theory/DormTest.java
+++ src/com/theory/DormTest.java
@@ -1,5 +1,47 @@
 package com.theory;
 
-public class DormTest {
+import java.util.Arrays;
+import java.util.Collections;
 
+public class DormTest {
+	//this is a test of the Comparable interface
+	/**public static void main(String[] args){
+		DormRoom[] cats = new DormRoom[20];
+		for(int i=0;i<20;i++){
+			int size = (int) (20*Math.random()+1);
+			System.out.println(size);
+			cats[i]=new DormRoom(size,1);
+		}
+		Collections.sort(Arrays.asList(cats));
+		for(int i=0;i<20;i++){
+			System.out.println(cats[i].getOccupancy());
+		}
+		
+	}**/
+	
+	//This is a test of the comparator interface
+	
+	public static void main (String[] args){
+		DormRoom[] cats= new DormRoom[20];
+		System.out.println("Floor -- Size");
+		for(int i=0;i<20;i++){
+			int floorNumber = (int) (20*Math.random()+1);
+			int size = (int) (20*Math.random()+1);
+			System.out.println(floorNumber+"    " +size);
+			cats[i]=new DormRoom(size,floorNumber);
+		}
+		Collections.sort(Arrays.asList(cats), DormRoom.compareByFloorNumber());
+		System.out.println("sorted by floor");
+		System.out.println("Floor -- Size");
+		for(int i=0;i<20;i++){
+			System.out.println(cats[i].getFloorNumber()+"    " +cats[i].getOccupancy());
+		}
+		Collections.sort(Arrays.asList(cats), DormRoom.compareByOccupancy());
+		System.out.println("sorted by occupancy");
+		System.out.println("Floor -- Size");
+		for(int i=0;i<20;i++){
+			System.out.println(cats[i].getFloorNumber()+"    " +cats[i].getOccupancy());
+		}
+	}
+	
 }
